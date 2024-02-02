2244fe8bff078fa690d643c5753beb2f0c9e705d
RaduTek <radutek13@gmail.com>
2024-02-02 23:13:38 +0200

    Move vector type definition to another file

--- XController/XController.cs
+++ XController/XController.cs
@@ -17,51 +17,6 @@ namespace XController
     }
 
     /// <summary>
-    /// 2D vector structure
-    /// </summary>
-    public struct Vector
-    {
-        /// <summary>
-        /// Horizontal coordinates relative to the center
-        /// </summary>
-        public double X;
-
-        /// <summary>
-        /// Vertical coordinates relative to the center
-        /// </summary>
-        public double Y;
-
-        /// <summary>
-        /// Create a new instance of the Vector structure
-        /// </summary>
-        public Vector(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        /// <summary>
-        /// Gets a string representation of the object
-        /// </summary>
-        /// <returns>A string with X and Y values</returns>
-        public override string ToString()
-        {
-            return $"X: {X:0.000}, Y: {Y:0.000}";
-        }
-
-        /// <summary>
-        /// Scale a vector object (2d vector)
-        /// </summary>
-        /// <param name="vector">Base vector</param>
-        /// <param name="scalar">Scalar</param>
-        /// <returns></returns>
-        public static Vector operator *(Vector vector, double scalar)
-        {
-            return new Vector(vector.X * scalar, vector.Y * scalar);
-    }
-    }
-
-    /// <summary>
     /// Trigger value structure
     /// </summary>
     public struct TriggerValue
